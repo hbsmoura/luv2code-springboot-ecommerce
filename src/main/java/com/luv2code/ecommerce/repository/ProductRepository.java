cbf75c212dbe3e47492a4d5a85b4134bb7388ca3
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2023-07-02 11:43:23 -0300

    enable search by category id

--- src/main/java/com/luv2code/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/repository/ProductRepository.java
@@ -1,11 +1,16 @@
 package com.luv2code.ecommerce.repository;
 
 import com.luv2code.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
